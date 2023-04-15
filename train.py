4e1ec33aca7dd33a1e74b0300ebc2dd0385c50a5
Igor Olkhovskiy <i.v.olkhovsky@gmail.com>
2023-04-15 14:52:39 +0300

    Add exception handler in trainining script

--- train.py
+++ train.py
@@ -64,74 +64,79 @@ def train(
     with tqdm(total=total_steps) as pbar:
         for epoch in range(epochs):
             for train_batch in train_dataloader:
-                model.train()
-                optimizer.zero_grad()
+                try:
+                    model.train()
+                    optimizer.zero_grad()
 
-                in_tokens = torch.Tensor(train_batch['encoder_input']).long().to(device)
-                dec_inputs = torch.Tensor(train_batch['decoder_input']).long().to(device)
-                dec_targets = torch.Tensor(train_batch['decoder_output']).long().to(device)
+                    in_tokens = torch.Tensor(train_batch['encoder_input']).long().to(device)
+                    dec_inputs = torch.Tensor(train_batch['decoder_input']).long().to(device)
+                    dec_targets = torch.Tensor(train_batch['decoder_output']).long().to(device)
 
-                loss, acc = model(
-                    tokens=in_tokens,
-                    dec_input=dec_inputs,
-                    dec_target=dec_targets,
-                )
+                    loss, acc = model(
+                        tokens=in_tokens,
+                        dec_input=dec_inputs,
+                        dec_target=dec_targets,
+                    )
+                    
+                    loss.backward()
+                    optimizer.step()
+
+                    if device != torch.device('cpu'):
+                        loss = loss.cpu()
+                        acc = acc.cpu()
+                    loss = loss.detach().item()
+                    acc = acc.detach().item()
+                    pbar.set_description(f'Step: {step} Epoch: {epoch} Loss: {loss} Acc: {acc}')
+                    writer.add_scalar('Loss/Train', loss, step)
+                    writer.add_scalar('Accuracy/Train', acc, step)
+                    
+                    with torch.no_grad():
+                        if val_steps and (step % val_steps == 0):
+                            val_batch = next(val_iter)
+                            in_tokens = val_batch['encoder_input']
+                            dec_inputs = val_batch['decoder_input']
+                            dec_targets = val_batch['decoder_output']
+
+                            val_result = ''
+                            for sample_idx in range(len(in_tokens)):
+                                text_input = tokenizer.decode_line(in_tokens[sample_idx]).replace('PAD', '')
+                                target_output = tokenizer.decode_line(dec_targets[sample_idx]).replace('PAD', '')
+                                prediction = model.infer(text_input, tokenizer)
+                                if len(prediction.replace(' ', '')):
+                                    val_result += f'Sample # {sample_idx} | {text_input} | {prediction} | ({target_output}) \n'
+
+                            if len(val_result):
+                                writer.add_text('Validation/Samples', val_result, global_step=step)
+
+                            model.train()
+                            val_loss, val_acc = model(
+                                tokens=torch.Tensor(in_tokens).to(model.device),
+                                dec_input=torch.Tensor(dec_inputs).to(model.device),
+                                dec_target=torch.Tensor(dec_targets).to(model.device),
+                            )
+                            if device != torch.device('cpu'):
+                                val_loss = val_loss.cpu()
+                                val_acc = val_acc.cpu()
+                            val_loss = val_loss.detach().item()
+                            val_acc = val_acc.detach().item()
+                            writer.add_scalar('Loss/Val', val_loss, step)
+                            writer.add_scalar('Accuracy/Val', val_acc, step)
+
+                    if autosave_tstamp is not None:
+                        if time() > autosave_tstamp + autosave_secs:
+                            autosave_tstamp = time()
+                            model_path = os.path.join(checkpoints_path, f"chatter_ep_{epoch}_step_{step}")
+                            model.save(model_path)
                 
-                loss.backward()
-                optimizer.step()
+                except Exception as e:
+                    print(f'Got exception during global step: {step}:\n{e}')
 
-                if device != torch.device('cpu'):
-                    loss = loss.cpu()
-                    acc = acc.cpu()
-                loss = loss.detach().item()
-                acc = acc.detach().item()
-                pbar.set_description(f'Step: {step} Epoch: {epoch} Loss: {loss} Acc: {acc}')
-                writer.add_scalar('Loss/Train', loss, step)
-                writer.add_scalar('Accuracy/Train', acc, step)
-                
-                with torch.no_grad():
-                    if val_steps and (step % val_steps == 0):
-                        val_batch = next(val_iter)
-                        in_tokens = val_batch['encoder_input']
-                        dec_inputs = val_batch['decoder_input']
-                        dec_targets = val_batch['decoder_output']
-
-                        val_result = ''
-                        for sample_idx in range(len(in_tokens)):
-                            text_input = tokenizer.decode_line(in_tokens[sample_idx]).replace('PAD', '')
-                            target_output = tokenizer.decode_line(dec_targets[sample_idx]).replace('PAD', '')
-                            prediction = model.infer(text_input, tokenizer)
-                            if len(prediction.replace(' ', '')):
-                                val_result += f'Sample # {sample_idx} | {text_input} | {prediction} | ({target_output}) \n'
-
-                        if len(val_result):
-                            writer.add_text('Validation/Samples', val_result, global_step=step)
-
-                        model.train()
-                        val_loss, val_acc = model(
-                            tokens=torch.Tensor(in_tokens).to(model.device),
-                            dec_input=torch.Tensor(dec_inputs).to(model.device),
-                            dec_target=torch.Tensor(dec_targets).to(model.device),
-                        )
-                        if device != torch.device('cpu'):
-                            val_loss = val_loss.cpu()
-                            val_acc = val_acc.cpu()
-                        val_loss = val_loss.detach().item()
-                        val_acc = val_acc.detach().item()
-                        writer.add_scalar('Loss/Val', val_loss, step)
-                        writer.add_scalar('Accuracy/Val', val_acc, step)
-                
                 pbar.update(1)
                 step += 1
 
-                if autosave_tstamp is not None:
-                    if time() > autosave_tstamp + autosave_secs:
-                        autosave_tstamp = time()
-                        model_path = os.path.join(checkpoints_path, f"chatter_ep_{epoch}_step_{step}")
-                        model.save(model_path)
-
                 if step >= total_steps:
                     break
+        
             if step >= total_steps:
                 break
     model_path = os.path.join(checkpoints_path, f"chatter_final_ep_{epoch}_step_{step}")
